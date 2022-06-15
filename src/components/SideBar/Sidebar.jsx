d22ca047d5cc3bc26672cd8e82d2502c339cde1b
AlanNJ <alannpn75@gmail.com>
2022-06-15 23:22:10 +0530

    build

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -3,32 +3,37 @@ import "../SideBar/sidebar.css";
 import { Link } from "react-router-dom";
 
 export const Sidebar = ({ openModal, setOpenModal }) => {
+	const doTask = (e) => {
+		e.preventDefault();
+		console.log("hello");
+		setOpenModal(!openModal);
+	};
 	return (
 		<>
 			{" "}
-			<div className="modal">
+			<div className="modal" onClick={(e) => setOpenModal(false)}>
 				<div class="sidebar">
 					<ul>
-						<li>
-							<Link to="/advisory" onClick={() => setOpenModal(false)}>
+						<li onClick={(e) => doTask(e)}>
+							<Link to="/advisory">
 								<a>Advisory</a>
 							</Link>
-							<Link to="/privacy-policy" onClick={() => setOpenModal(false)}>
+							<Link to="/privacy-policy">
 								<a>Privacy Policy</a>
 							</Link>
-							<Link to="/downloads" onClick={() => setOpenModal(false)}>
+							<Link to="/downloads">
 								<a>Downloads</a>
 							</Link>
-							<Link to="/partner-with-us" onClick={() => setOpenModal(false)}>
+							<Link to="/partner-with-us">
 								<a>Partner With Us</a>
 							</Link>
-							<Link to="/services" onClick={() => setOpenModal(false)}>
+							<Link to="/services">
 								<a>Services</a>
 							</Link>
-							<Link to="/trading" onClick={() => setOpenModal(false)}>
+							<Link to="/trading">
 								<a>Trading</a>
 							</Link>
-							<Link to="/payments" onClick={() => setOpenModal(false)}>
+							<Link to="/payments">
 								<a>Payments</a>
 							</Link>
 						</li>
